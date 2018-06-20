c2dc6167a18bf8b7f34a510399afedbed9cbc97b
Ikey Benzaken <ikey.benz@gmail.com>
2018-06-20 18:25:27 -0400

    pls work

--- app.js
+++ app.js
@@ -255,8 +255,8 @@ function getMyPDF() {
             if (data.slice(0, 13) == "We appologize") {
                 alert(data);
             } else {
-                if (!!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform)) {
-                    alert('this is an iphone');
+                if((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i)) || (navigator.userAgent.match(/iPad/i))) {
+                    alert('this is an iOS device');
                     $('#downloadLink').prop('href', `data:application/pdf;base64,${data}`);
                     $('downloadPopup').show();
                 } else {
