03bb0edf321601cdfaf9e63a8afa99d882ed3584
Ikey Benzaken <ikey.benz@gmail.com>
2018-10-09 14:45:53 -0700

    Added Support for sorting archives by rabbi

--- Archives/app.js
+++ Archives/app.js
@@ -9,67 +9,104 @@ var config = {
 firebase.initializeApp(config);
 var database = firebase.database();
 
-function loadArchives() {
-    return new Promise(async function(resolve, reject) {
-        let snapshot = await database.ref('SubcriptionOptions').once('value');
-        let subscriptions = snapshot.val();
-        let archives = {}
-        for (let subKey in subscriptions) {
-            let sub = subscriptions[subKey];
-            if (sub.Archives) {
-                archives[`${sub.Author}`] = sub.Archives;
-            }
+$(document).ready(function() {
+    Archives.displayByMonth();
+    $('#sortPreference').on('change', function() {
+        console.log('Changed!');
+        if (this.value == 'Rabbi') {
+            Archives.displayByRabbi();
+        } else {
+            Archives.displayByMonth();
         }
-        resolve(organizeArchives(archives));
     });
-}
-async function organizeArchives(archives) {
-    let months = ["January", 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    let archivesByMonth = {}
-    for (let rabbiName in archives) {
-        for (title in archives[rabbiName]) {
-            let info = title.split('-');
-            let month = months[info[1] - 1];
-            let parasha = info[0];
-            if (!archivesByMonth[month]) {
-                archivesByMonth[month] = {}
-            }
-            if (!archivesByMonth[month][parasha]) {
-                archivesByMonth[month][parasha] = {}
-            }
-            archivesByMonth[month][parasha][rabbiName] = archives[rabbiName][title]
-        }
-    }
-    return archivesByMonth
-}
-function displayArchives(sortedArchives) {
-    var html = "";
-    console.log(sortedArchives);
-    for (let month in sortedArchives) {
-        console.log(month);
-        html += `<h2>${month}:</h2>`;
-        for (let parasha in sortedArchives[month]) {
-            html += `
-                <div class="archiveContainer">
-                    <h3>Parashat ${parasha}:</h3>
-                    <div class="archiveSlider">`;
-            for (let rabbi in sortedArchives[month][parasha]) {
-                html += `
-                    <a class="previewContainer" href="${sortedArchives[month][parasha][rabbi]}">
-                        <img src="${sortedArchives[month][parasha][rabbi]}">
-                        <p>${rabbi}</p>
-                    </a>`;
-            }
-            html += '</div></div>';    
-        }
-    }
-    $('main').append(html);
-    console.log(html);
-}
+});
 
-async function main() {
-    let archives = await loadArchives();
-    displayArchives(archives);
-    console.log('This happend')
-}
-$(document).ready(main)
+const Archives = (function() {
+    var loadedArchives;
+    function load() {
+        if (!loadedArchives) {
+            return new Promise(async function(resolve, reject) {
+                let snapshot = await database.ref('SubcriptionOptions').once('value');
+                let subscriptions = snapshot.val();
+                let a = {}
+                for (let subKey in subscriptions) {
+                    let sub = subscriptions[subKey];
+                    if (sub.Archives) {
+                        a[`${sub.Author}`] = sub.Archives;
+                    }
+                }
+                loadedArchives = a;
+                resolve(a);
+            });
+        } 
+        return loadedArchives;
+    }
+    function organizeByMonth(archives) {
+        let months = ["January", 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        let archivesByMonth = {}
+        for (let rabbiName in archives) {
+            for (title in archives[rabbiName]) {
+                let info = title.split('-');
+                let month = months[info[1] - 1];
+                let parasha = info[0];
+                if (!archivesByMonth[month]) {
+                    archivesByMonth[month] = {}
+                }
+                if (!archivesByMonth[month][parasha]) {
+                    archivesByMonth[month][parasha] = {}
+                }
+                archivesByMonth[month][parasha][rabbiName] = archives[rabbiName][title]
+            }
+        }
+        return archivesByMonth;
+    }
+    function showHTMLByMonth(sortedArchives) {
+        var html = "";
+        for (let month in sortedArchives) {
+            html += `<h2>${month}:</h2>`;
+            for (let parasha in sortedArchives[month]) {
+                html += `
+                        <div class="archiveContainer">
+                            <h3>Parashat ${parasha}:</h3>
+                            <div class="archiveSlider">`;
+                for (let rabbi in sortedArchives[month][parasha]) {
+                    html += `
+                            <a class="previewContainer" href="${sortedArchives[month][parasha][rabbi]}">
+                                <img src="${sortedArchives[month][parasha][rabbi]}">
+                                <p>${rabbi}</p>
+                            </a>`;
+                }
+                html += '</div></div>';    
+            }
+        }
+        $('main').html(html);
+    }
+    function showHTMLByRabbi(archives) {
+        let html = '';
+        for (let rabbiName in archives) {
+            html += `<h2>${rabbiName}:</h2>
+                     <div class="archiveContainer">
+                     <div class="archiveSlider">`;
+            for (let title in archives[rabbiName]) {
+                html += `<a class="previewContainer" href="${archives[rabbiName][title]}">
+                            <img src="${archives[rabbiName][title]}">
+                            <p>${title.slice(0, title.indexOf('-'))}</p>
+                         </a>`;
+            }
+            html += '</div></div>';
+        }
+        $('main').html(html);
+    }
+
+    return {
+        displayByMonth: async function() {
+            const a = await load();
+            const organized = organizeByMonth(a);
+            showHTMLByMonth(organized);
+        },
+        displayByRabbi: async function() {
+            const a = await load();
+            showHTMLByRabbi(a);
+        }
+    }
+})();
